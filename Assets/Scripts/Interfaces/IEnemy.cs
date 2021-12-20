7b0188c31d87f7d31ccb59ef3789f6447114d30e
osx <osx@MacBook-Air-di-osx-8.local>
2021-12-20 16:45:34 +0100

    add enemybehaviour

---
+++ Assets/Scripts/Interfaces/IEnemy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IEnemy
+{
+    void Chase(GameObject obj);
+
+    void FleeFrom(GameObject obj);
+   
+}
