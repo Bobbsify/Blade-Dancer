28314b963e03a59e69c8c723f3854a44503b4b02
osx <osx@MacBook-Air-di-osx-8.local>
2021-12-20 18:10:45 +0100

    add dashAndShoot

---
+++ Assets/Scripts/Abilities/Dash.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour
+{
+    private bool canDash;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float DashCountdown;
+
+    public IEnumerator CountdownDash()
+    {
+        yield return new WaitForSeconds(this.DashCountdown);
+        this.canDash = true;
+
+    }
+
+    public void Trigger(GameObject obj)
+    {
+        if (this.canDash)
+        {
+            // TODO dash character
+            this.canDash = false;
+            StartCoroutine(CountdownDash());
+        }
+    }
+
+    public void SendActionToGameManager()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Abilities/Shoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour, IAbility
+{
+    private bool canShooting;
+
+    [SerializeField]
+    private float shootCountdown;
+
+    public IEnumerator CountdownShooting()
+    {
+        yield return new WaitForSeconds(this.shootCountdown);
+        this.canShooting = true;
+
+    }
+
+    public void Trigger(GameObject obj)
+    {
+        if (this.canShooting)
+        {
+            //TODO spara il proiettile
+            this.canShooting = false;
+            StartCoroutine(CountdownShooting());
+        }
+    }
+
+    public void SendActionToGameManager()
+    {
+
+    } 
+}
