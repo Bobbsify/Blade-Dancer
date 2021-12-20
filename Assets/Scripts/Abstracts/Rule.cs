dbfeb8b9980ead87107563937af73f39c1e75690
Matteo Perini <17-13385@saeinstitute.edu>
2021-12-20 17:47:15 +0100

    create RuleObjects and Abstract rule

---
+++ Assets/Scripts/Abstracts/Rule.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Rule
+{
+    List<IAction> appliedActions; //azioni che causano il completamento della regola
+    List<IRule> mutuallyExclusives; // regole con cui non può essere associata questa regola(Bidirezionale)
+    List<RuleObject> ruleRelatedObjects; // (nullable) eventuale oggetti correlati all’utilizzo della regola
+    float durationMultiplier; // moltiplicatore che definisce la quantità in secondi di cui deve aumentare la durata in presenza di questa regola
+
+}
--- Assets/Scripts/Interfaces/IRule.cs
+++ Assets/Scripts/Interfaces/IRule.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public interface IRule
 {
+    //Checks if current action completes the rule
     bool CheckAction(IAction action);
 }
