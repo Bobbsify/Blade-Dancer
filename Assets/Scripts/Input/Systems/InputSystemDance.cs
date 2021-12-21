db294600d684d4e95a3d1228960e84cfe8abee1b
osx <osx@MacBook-Air-di-osx-8.local>
2021-12-21 17:03:42 +0100

    add receiverDance

---
+++ Assets/Scripts/Input/Systems/InputSystemDance.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputManager))]
+public class InputSystemDance : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject searchRoot;
+
+    [SerializeField]
+    private string danceAxisName;
+
+    private IInputReceiverDance[] receivers;
+
+    private void OnValidate()
+    {
+        InputManager manager;
+        if (TryGetComponent(out manager))
+        {
+            this.searchRoot = manager.GetRoot();
+        }
+    }
+
+    private void Start()
+    {
+        this.receivers = this.searchRoot.GetComponentsInChildren<IInputReceiverDance>(true);
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxisRaw(danceAxisName) > 0f)
+        {
+            for (int i = 0; i < this.receivers.Length; i++)
+            {
+                IInputReceiverDance receiver = this.receivers[i];
+                receiver.ReceiveInputDance();
+            }
+        }
+    }
+}
