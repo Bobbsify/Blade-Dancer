4e62de576b7ca7f5e841cf438708ea7713be23d3
Antonio Filasieno <17-13379@saeinstitute.edu>
2021-12-21 16:08:06 +0100

    update SoundPacket

--- Assets/Scripts/Sound system/SoundPacket.cs
+++ Assets/Scripts/Sound system/SoundPacket.cs
@@ -6,8 +6,41 @@ public class SoundPacket
 {
     private AudioSource audio;
     private Transform playPos;
-    private AudioType soundType;
+    private SoundType audioType;
     private float playDelay;
 
+    SoundPacket(AudioSource audio, Transform playPos, SoundType audioType, float playDelay = 0)
+    {
+        this.audio = audio;
+        this.playPos = playPos;
+        this.audioType = audioType;
+        this.playDelay = playDelay;
+    }
 
+    public AudioSource GetAudio()
+    {
+        return audio;
+    }
+
+    public Transform GetPlayPosition()
+    {
+        return playPos;
+    }
+
+    public SoundType GetType()
+    {
+        return audioType;
+    }
+
+    public float GetDelay()
+    {
+        return playDelay;
+    }
+
+}
+public enum SoundType
+{
+    Loop,
+    PlayOnce,
+    ReplayAfterSeconds
 }
