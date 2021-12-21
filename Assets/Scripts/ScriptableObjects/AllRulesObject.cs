4d3679e8d093b1c90f49cffaed552648a9c72654
Bobbsify <ultra-matty@hotmail.com>
2021-12-21 17:00:50 +0100

    add AllRules scriptable object

---
+++ Assets/Scripts/ScriptableObjects/AllRulesObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Rules", menuName = "ScriptableObjects/AllRules", order = 0)]
+public class AllRulesObject : ScriptableObject
+{
+    public MoveRule moveRule;
+    public NotMoveRule doNotMoveRule;
+    public ShootRule shootRule;
+    public NotShootRule doNotShootRule;
+}
