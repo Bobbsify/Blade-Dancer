ed393d6d916add522d5a01bf9c13dd9eb6155444
Bobbsify <ultra-matty@hotmail.com>
2021-12-21 15:13:54 +0100

    add PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour, IInputReceiverMove
+{
+    List<IAbility> abilities = new List<IAbility>();
+
+    [SerializeField]
+    private float speed = 5.0f;
+
+    private void Awake()
+    {
+    }
+
+    public void ReceiveInputMove(Vector3 direction)
+    {
+        this.transform.position += direction * Time.deltaTime * speed;
+    }
+}
