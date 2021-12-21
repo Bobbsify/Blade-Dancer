9f2c63e51e91fa4567caf326e92fd962bf40a436
osx <osx@MacBook-Air-di-osx-8.local>
2021-12-21 16:09:48 +0100

    add inputDash

---
+++ Assets/Scripts/Enemies/MeleeEnemyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemyController : MonoBehaviour,IEnemy
+{
+    [SerializeField]
+    private float speed;
+
+    private GameObject player;
+
+    public bool Act;
+
+    private string playerTag = "Player";
+
+    private void Update()
+    {
+        if (Act)
+        {
+            Chase(player);
+        }     
+    }
+
+    public void GetPlayer()
+    {
+        this.player = GameObject.FindGameObjectWithTag(playerTag);
+    }
+
+    public void Chase(GameObject obj)
+    {
+        this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, obj.transform.position, this.speed * Time.deltaTime);
+    }
+
+    public void FleeFrom(GameObject obj)
+    {
+
+    }
+}
