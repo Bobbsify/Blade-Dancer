2730934abd7e84a580fe27d34310c31a9cd202a7
osx <osx@MacBook-Air-di-osx-8.local>
2021-12-22 18:09:06 +0100

    add pause

--- Assets/Scripts/Abilities/Shoot.cs
+++ Assets/Scripts/Abilities/Shoot.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerController))]
 public class Shoot : MonoBehaviour, IAbility
 {
     private bool canShooting;
---
+++ Assets/Scripts/Input/Systems/InputSystemPause.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputManager))]
+public class InputSystemPause : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject searchRoot;
+
+    [SerializeField]
+    private string pauseAxisName;
+
+    private IInputReceiverPause[] receivers;
+
+    private void OnValidate()
+    {
+        InputManager manager;
+
+        if (TryGetComponent(out manager))
+        {
+            this.searchRoot = manager.GetRoot();
+        }
+    }
+
+    private void Start()
+    {
+        this.receivers = this.searchRoot.GetComponentsInChildren<IInputReceiverPause>(true);
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxisRaw(pauseAxisName) > 0f)
+        {
+            for (int i = 0; i < this.receivers.Length; i++)
+            {
+                IInputReceiverPause receiver = this.receivers[i];
+                receiver.ReceiveInputPause();
+            }
+        }
+    }
+}
