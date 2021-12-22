2f7918d53f2f71d6d0660a48b9fe035bec28e352
Bobbsify <ultra-matty@hotmail.com>
2021-12-23 00:18:27 +0100

    update ruleObject logic

--- Assets/Scripts/ScriptableObjects/AllRulesObject.cs
+++ Assets/Scripts/ScriptableObjects/AllRulesObject.cs
@@ -6,8 +6,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Rules", menuName = "ScriptableObjects/AllRules", order = 0)]
 public class AllRulesObject : ScriptableObject
 {
+    [Header("-- Move --")]
     public MoveRule moveRule;
     public NotMoveRule doNotMoveRule;
+
+    [Header("-- Shoot --")]
     public ShootRule shootRule;
     public NotShootRule doNotShootRule;
+
+    /* [Header("-- Dash --")]
+
+     [Header("-- Dance --")]
+     */
 }
