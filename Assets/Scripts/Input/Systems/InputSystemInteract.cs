760351a1a0ab9ab7335750fbbe7fffdca21bc373
osx <osx@MacBook-Air-di-osx-8.local>
2021-12-23 17:29:36 +0100

    add inputInter

---
+++ Assets/Scripts/Input/Systems/InputSystemInteract.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputManager))]
+public class InputSystemInteract : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject searchRoot;
+
+    [SerializeField]
+    private string interactAxisName;
+
+    private IInputReceiverInteract[] receivers;
+
+    private void OnValidate()
+    {
+        InputManager manager;
+        if (TryGetComponent(out manager))
+        {
+            this.searchRoot = manager.GetRoot();
+        }
+    }
+
+    private void Start()
+    {
+        this.receivers = this.searchRoot.GetComponentsInChildren<IInputReceiverInteract>(true);
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxisRaw(interactAxisName) > 0f)
+        {
+            for (int i = 0; i < this.receivers.Length; i++)
+            {
+                IInputReceiverInteract receiver = this.receivers[i];
+                receiver.ReceiveInputInteract();
+            }
+        }
+    }
+}
