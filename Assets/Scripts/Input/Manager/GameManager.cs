bd02013dabd8d3af65842ddfb57a83666ac731cf
osx <osx@MacBook-Air-di-osx-8.local>
2021-12-24 10:00:18 +0100

    add fsm

---
+++ Assets/Scripts/Input/Manager/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject searchRoot;
+
+	[SerializeField]
+	private Transform inputReceiversTransform;
+
+	[SerializeField]
+	private Transform spawnPoint;
+
+	[SerializeField]
+	private GameObject playerPrefab;
+
+	public GameObject PlayerPawn { get; private set; }
+
+	private void Awake()
+	{
+		this.GeneratePlayerPawn();
+	}
+
+	private void Start()
+	{
+		var entities = this.searchRoot.GetComponentsInChildren<IGameEntity>();
+		for (int i = 0; i < entities.Length; i++)
+		{
+			var entity = entities[i];
+			entity.Init(this);
+		}
+	}
+
+	private void GeneratePlayerPawn()
+	{
+		this.PlayerPawn = GameObject.Instantiate(this.playerPrefab, this.spawnPoint.position, Quaternion.identity);
+		this.PlayerPawn.transform.SetParent(this.inputReceiversTransform);
+	}
+}
