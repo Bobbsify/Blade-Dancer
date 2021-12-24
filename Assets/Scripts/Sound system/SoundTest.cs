3fc685f60b8fa41018aabfa356495009e963ad4b
Antonio Filasieno <17-13379@saeinstitute.edu>
2021-12-24 11:20:08 +0100

    add sound

---
+++ Assets/Scripts/Sound system/SoundTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundTest : MonoBehaviour
+{
+    [SerializeField] SoundPacket test;
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.E))
+        {
+            GetAudioType();
+        }
+    }
+
+    void GetAudioType()
+    {
+
+    }
+}
