77a076247621588aef9eeb98689f8b3196820d23
Bobbsify <ultra-matty@hotmail.com>
2021-12-24 10:18:03 +0100

    empty playerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,40 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour, IInputEntity, IInputReceiverMove, IInputReceiverShoot
+public class PlayerController : MonoBehaviour
 {
-    List<IAbility> abilities = new List<IAbility>();
-    IInputManager inputManager;
-
-    Dash dashAbility = new Dash();
-    Shoot shootAbility = new Shoot();
-
-    [SerializeField]
-    private float speed = 5.0f;
-
-    private void Awake()
-    {
-        abilities.Add(dashAbility);
-        abilities.Add(shootAbility);
-    }
-
-    public void ReceiveInputMove(Vector3 direction)
-    {
-        this.transform.position += direction * Time.deltaTime * speed;
-    }
-
-    public void InitInput(IInputManager inputManager)
-    {
-        this.inputManager = inputManager;
-    }
-
-    public void ReceiveInputRangedAttack()
-    {
-        dashAbility.Trigger(gameObject);
-    }
-
-    public void ReceiveInputShoot()
-    {
-       
-    }
 }
