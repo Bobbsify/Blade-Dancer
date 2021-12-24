6fcee427b4c9a05ea4e849589bc79a889b1ef2d3
Bobbsify <ultra-matty@hotmail.com>
2021-12-24 10:16:45 +0100

    add negative Move Dash Dance and Shoot

---
+++ Assets/Scripts/Rules/Negatives/NotDanceRule.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class NotDanceRule : Rule
+{
+    private float durationModFormula => 1.0f;
+
+    private bool complete = true;
+
+    public NotDanceRule()
+    {
+
+        switch (GetDurationModType())
+        {
+            case DurationModType.RuleDependant:
+                SetDurationMod(durationModFormula);
+                break;
+            case DurationModType.FixedAmount:
+                //Duration is already set in inspector
+                break;
+            default:
+                throw new System.InvalidOperationException("Unkown durationModType: " + GetDurationModType());
+        }
+    }
+
+
+    public override bool CheckAction(Actions executedAction)
+    {
+        foreach (Actions action in appliedActions)
+        {
+            if (action == executedAction)
+            {
+                complete = false;
+                return complete;
+            }
+        }
+        return true;
+    }
+
+    public override bool IsRuleComplete()
+    {
+        return complete;
+    }
+
+    public override string ToString()
+    {
+        return "Dance";
+    }
+}
---
+++ Assets/Scripts/Rules/Negatives/NotMoveRule.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class NotMoveRule : Rule
+{
+    bool hasNotMoved = true;
+
+    private float durationModFormula => -2f;
+
+    public NotMoveRule()
+    {
+
+        switch (GetDurationModType())
+        {
+            case DurationModType.RuleDependant:
+                SetDurationMod(durationModFormula);
+                break;
+            case DurationModType.FixedAmount:
+                //Duration is already set in inspector
+                break;
+            default:
+                throw new System.InvalidOperationException("Unkown durationModType: " + GetDurationModType());
+        }
+    }
+
+    public override bool CheckAction(Actions exectuedAction)
+    {
+        foreach (Actions action in appliedActions)
+        {
+            if (action == exectuedAction)
+            {
+                hasNotMoved = false;
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public override bool IsRuleComplete()
+    {
+        return hasNotMoved;
+    }
+
+    public override string ToString()
+    {
+        return "Move";
+    }
+}
