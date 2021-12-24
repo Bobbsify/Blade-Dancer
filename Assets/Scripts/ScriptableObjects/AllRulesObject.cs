3d8eb66ebe02b579a152a896439558a587e8deca
Bobbsify <ultra-matty@hotmail.com>
2021-12-24 10:18:28 +0100

    edit funcionality of Rules

--- Assets/Scripts/ScriptableObjects/AllRulesObject.cs
+++ Assets/Scripts/ScriptableObjects/AllRulesObject.cs
@@ -6,16 +6,38 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Rules", menuName = "ScriptableObjects/AllRules", order = 0)]
 public class AllRulesObject : ScriptableObject
 {
-    [Header("-- Move --")]
+    [Header("-------- Move --------")]
     public MoveRule moveRule;
     public NotMoveRule doNotMoveRule;
 
-    [Header("-- Shoot --")]
+    [Header("-------- Shoot --------")]
     public ShootRule shootRule;
     public NotShootRule doNotShootRule;
 
-    /* [Header("-- Dash --")]
+    [Header("-- Dash --")]
+    public DashRule dashRule;
+    public NotDashRule doNotDashRule;
 
-     [Header("-- Dance --")]
-     */
+    [Header("-------- Dance --------")]
+    public DanceRule danceRule;
+    public NotDanceRule doNotDanceRule;
+
+    public List<Rule> getAll()
+    {
+        List<Rule> allRules = new List<Rule>();
+
+        allRules.Add(moveRule);
+        allRules.Add(doNotMoveRule);
+
+        allRules.Add(shootRule);
+        allRules.Add(doNotShootRule);
+
+        allRules.Add(dashRule);
+        allRules.Add(doNotDashRule);
+
+        allRules.Add(danceRule);
+        allRules.Add(doNotDanceRule);
+
+        return allRules;
+    }
 }
