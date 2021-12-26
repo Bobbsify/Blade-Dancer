f694d9741b9171047d16d2c48c7f006594d98c7f
osx <osx@MacBook-Air-di-osx-8.local>
2021-12-26 12:10:22 +0100

    add Navmeshrequired

---
+++ Assets/Scripts/EnemiesAndAI/EnemieNoNavMesh/RangedEnemyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedEnemyController : MonoBehaviour, IEnemy
+{
+    [SerializeField]
+    private float speed;
+
+    private GameObject player;
+
+    public bool Act;
+
+    private string playerTag = "Player";
+
+    private void Update()
+    {
+        if (Act)
+        {
+            FleeFrom(player);
+        }
+    }
+
+    public void GetPlayer()
+    {
+        this.player = GameObject.FindGameObjectWithTag(playerTag);
+    }
+
+    public void Chase(GameObject obj)
+    {
+       
+    }
+
+    public void FleeFrom(GameObject obj)
+    {
+        this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, -(obj.transform.position), this.speed * Time.deltaTime);
+    }
+}
