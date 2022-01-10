d041cdbc3c02f8a537426eec38a6b9a61c78c838
osx <osx@MacBook-Air-di-osx-8.local>
2022-01-10 17:22:29 +0100

    fix ballObject

--- Assets/Prefabs/RuleObjects/BallObject.cs
+++ Assets/Prefabs/RuleObjects/BallObject.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class BallObject : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
