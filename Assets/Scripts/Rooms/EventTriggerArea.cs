35f18991842f55c6aa41492411c3dac8f7522184
Bobbsify <ultra-matty@hotmail.com>
2022-01-10 18:31:24 +0100

    add event trigger

---
+++ Assets/Scripts/Rooms/EventTriggerArea.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class EventTriggerArea : MonoBehaviour
+{
+    [SerializeField]
+    private TriggerMode triggerMode;
+
+    [SerializeField]
+    private UnityEvent eventsToTrigger;
+
+    private Collider col;
+    private void Awake()
+    {
+        TryGetComponent(out col);
+        col.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out PlayerController pc)) 
+        {
+            if (triggerMode == TriggerMode.TriggerOnce)
+            { 
+                this.enabled = false;
+            }
+            eventsToTrigger.Invoke();
+        }
+    }
+}
+
+public enum TriggerMode 
+{ 
+    TriggerOnce,
+    AlwaysTrigger
+}
