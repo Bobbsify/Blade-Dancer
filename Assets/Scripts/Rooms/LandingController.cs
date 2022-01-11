f21041e851e224453e457275835b5e315e63d55c
Bobbsify <ultra-matty@hotmail.com>
2022-01-11 17:58:55 +0100

    add landing check

---
+++ Assets/Scripts/Rooms/LandingController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class LandingController : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent(out PlayerController pc)) 
+        {
+            pc.EnableAllAbilities();
+            this.enabled = false;
+        }
+    }
+}
