99cf3847653fb6f425126e24f258a19d36c5d9dd
Bobbsify <ultra-matty@hotmail.com>
2022-01-11 21:30:25 +0100

    add new Game scene loading

---
+++ Assets/Scripts/Scene/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void GoToNextScene() 
+    {
+        int nextScene = SceneManager.GetActiveScene().buildIndex+1;
+        SceneManager.LoadScene(nextScene);
+    }
+}
