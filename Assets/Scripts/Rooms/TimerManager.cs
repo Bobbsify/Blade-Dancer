a78be2e94197cec70c4cd2a5c34de1c042aa2576
Bobbsify <ultra-matty@hotmail.com>
2022-01-17 14:55:01 +0100

    add Timer to game

---
+++ Assets/Scripts/Rooms/TimerManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerManager : MonoBehaviour, IGameEntity
+{
+    private GameManager gameManager;
+
+    private float maxTime;
+
+    private float currentTime;
+
+    private bool doTimer = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doTimer) {         
+            currentTime -= Time.deltaTime;
+            Debug.Log(currentTime);
+            if (currentTime < 0) 
+            {
+                doTimer = false;
+                gameManager.KillPlayer();
+            }
+        }
+    }
+
+    public void SetTimer(float amount) 
+    {
+        currentTime = amount;
+        doTimer = true;
+    }
+
+    public void StopTimer()
+    {
+        doTimer = false;
+    }
+
+    public void StartTimer()
+    {
+        doTimer = true;
+    }
+
+    public void Init(GameManager gameManager)
+    {
+        this.gameManager = gameManager;
+    }
+}
