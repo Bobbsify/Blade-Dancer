fe4c51a0861d872f10d53d1f039830525b2f5313
Bobbsify <ultra-matty@hotmail.com>
2022-01-19 10:57:38 +0100

    add volume management script

---
+++ Assets/Scripts/UI/Settings/VolumeManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class VolumeManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixerGroup mixer;
+
+    private Slider volume;
+
+    private float currentVolume;
+
+    private void Awake() 
+    {
+        mixer.audioMixer.GetFloat(mixer.name, out currentVolume);
+        volume.value = currentVolume;
+    }
+
+    public void ChangeVolume(float value) 
+    {
+        currentVolume = value;
+        mixer.audioMixer.SetFloat(mixer.name, currentVolume);
+    }
+}
