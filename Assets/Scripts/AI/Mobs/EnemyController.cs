981f9af0aa562ac0a51db2d3a3423524709f17c4
Bobbsify <ultra-matty@hotmail.com>
2022-01-21 11:16:18 +0100

    add Enemy death

---
+++ Assets/Scripts/AI/Mobs/EnemyController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(FSM))]
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField]
+    private float maxHealth = 1;
+
+    private float currentHealth;
+
+    private Animator animator;
+
+    private FSM stateMachine;
+
+    private void Start()
+    {
+        TryGetComponent(out stateMachine);
+        TryGetComponent(out animator);
+    }
+
+    public void TakeDamage(int amount) 
+    {
+        currentHealth = Mathf.Max(currentHealth - amount, 0);
+        if (currentHealth == 0) 
+        {
+            Die();
+        }
+    }
+
+    public void DestroyEnemy() 
+    {
+        Destroy(gameObject);
+    }
+
+    private void Die() 
+    {
+        stateMachine.DisalbeStates();
+        animator.SetTrigger("death");
+    }
+}
