31e5521a7c03b1e8668f768e4e0284e477547e72
osx <osx@MacBook-Air-di-osx-8.local>
2022-02-02 17:23:56 +0100

    add soundstrak audiosources

---
+++ Assets/Scripts/Sound system/StreakMusic.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StreakMusic : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource[] audioSources;
+
+    private List<AudioSource> sources = new List<AudioSource>();
+
+    private int RandomMusicSelector;
+
+    private void Start()
+    { 
+        CompileSources();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            GetSong();
+        }
+    }
+
+    public AudioSource GetSong()
+    {
+        AudioSource[] audios = this.audioSources;
+
+        if(sources.Count < 1)
+        {
+            CompileSources();
+        }
+
+        do
+        {
+            RandomMusicSelector = Random.Range(0, audioSources.Length);
+        } while (!sources.Contains(audios[RandomMusicSelector]));
+
+         sources.Remove(audios[RandomMusicSelector]);
+      
+         audios[RandomMusicSelector].Play();
+            
+         return audios[RandomMusicSelector];
+    }
+
+    private void CompileSources()
+    {
+       for (int i = 0; i < audioSources.Length; i++)
+       {
+           sources.Add(audioSources[i]);
+       }
+    }
+}
