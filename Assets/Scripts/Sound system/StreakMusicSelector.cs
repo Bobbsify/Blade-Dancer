f6efc03bd1de0e7963800411d0b04868649c6562
Bobbsify <ultra-matty@hotmail.com>
2022-02-03 12:17:27 +0100

    fix streakMusicSelector

--- Assets/Scripts/Sound system/StreakMusicSelector.cs
+++ Assets/Scripts/Sound system/StreakMusicSelector.cs
@@ -8,28 +8,42 @@ public class StreakMusicSelector : MonoBehaviour
     [SerializeField]
     private SoundPacket[] soundPackets;
 
-    private List<int> lastTwoSongIndex = new List<int>();
+    [SerializeField]
+    [Range(2,5)]
+    private int maxMusic = 2;
 
-    private const int maxMusic = 2;
+    private List<int> lastTwoSongIndex = new List<int>();
    
-    private int getRandomPacketNumber;
+    private int randomPacketIndex;
 
     public SoundPacket GetMusic()
     {
-        if(lastTwoSongIndex.Count > maxMusic)
-        {
-            lastTwoSongIndex.Remove(lastTwoSongIndex[1]);
-        }
 
         do
         {
-            getRandomPacketNumber = UnityEngine.Random.Range(0, soundPackets.Length);
-        } while (lastTwoSongIndex.Contains(getRandomPacketNumber));
+            randomPacketIndex = UnityEngine.Random.Range(0, soundPackets.Length);
+        } while (lastTwoSongIndex.Contains(randomPacketIndex));
 
-        lastTwoSongIndex.Add(getRandomPacketNumber);
-        lastTwoSongIndex[1] = lastTwoSongIndex[0];
-        lastTwoSongIndex[0] = lastTwoSongIndex[getRandomPacketNumber];
+        ShiftIndexList();
 
         return soundPackets[lastTwoSongIndex[0]];
+    }
+
+    private void ShiftIndexList()
+    {
+        //Remove Last Element
+        lastTwoSongIndex.Remove(lastTwoSongIndex[lastTwoSongIndex.Count - 1]);
+
+        //Shift All
+        if (lastTwoSongIndex.Count > 0) 
+        { 
+            for (int i = maxMusic-1; i != 1; i--) 
+            {
+                lastTwoSongIndex[i] = lastTwoSongIndex[i - 1];
+            }
+        }
+
+        //Add Element
+        lastTwoSongIndex[0] = randomPacketIndex;
     }
 }
