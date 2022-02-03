5b8ee37ce461ff816107c66d65e57fc87b49e8d6
Bobbsify <ultra-matty@hotmail.com>
2022-02-03 12:38:03 +0100

    fix selection

--- Assets/Scripts/Sound system/StreakMusicSelector.cs
+++ Assets/Scripts/Sound system/StreakMusicSelector.cs
@@ -9,13 +9,23 @@ public class StreakMusicSelector : MonoBehaviour
     private SoundPacket[] soundPackets;
 
     [SerializeField]
-    [Range(2,5)]
+    [Range(1,5)]
     [Tooltip("Amount of songs needed for a song to be able to be repeated")]
     private int repeatRange = 2;
 
-    private List<int> repeatedSongsIndexes = new List<int>();
+    private int[] repeatedSongsIndexes;
    
     private int randomPacketIndex;
+
+    private void Awake()
+    {
+        repeatedSongsIndexes = new int[repeatRange];
+        //Get all numbers out of possible range
+        for (int i = 0; i < repeatedSongsIndexes.Length; i++) 
+        {
+            repeatedSongsIndexes[i] = -1; 
+        }
+    }
 
     public SoundPacket GetMusic()
     {
@@ -23,34 +33,33 @@ public class StreakMusicSelector : MonoBehaviour
         do
         {
             randomPacketIndex = UnityEngine.Random.Range(0, soundPackets.Length);
-        } while (repeatedSongsIndexes.Contains(randomPacketIndex));
+        } while (CheckRepeated());
 
         ShiftIndexList();
 
-        if (repeatedSongsIndexes.Count > 0)
-        {
-            repeatedSongsIndexes[0] = randomPacketIndex;
-        }
-        else 
-        {
-            repeatedSongsIndexes.Add(randomPacketIndex);
-        }
+        repeatedSongsIndexes[0] = randomPacketIndex;
 
         return soundPackets[repeatedSongsIndexes[0]];
     }
 
+    private bool CheckRepeated()
+    {
+        foreach (int i in repeatedSongsIndexes) 
+        {
+            if (randomPacketIndex == i) 
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
     private void ShiftIndexList()
     {
-        //Remove Last Element
-        if (repeatedSongsIndexes.Count > 0)
+        //Shift All
+        for (int i = repeatRange-1; i != 0 && repeatRange > 1; i--) 
         {
-            repeatedSongsIndexes.Remove(repeatedSongsIndexes[repeatedSongsIndexes.Count - 1]);
-
-            //Shift All
-            for (int i = repeatRange-1; i != 1; i--) 
-            {
-                repeatedSongsIndexes[i] = repeatedSongsIndexes[i - 1];
-            }
+            repeatedSongsIndexes[i] = repeatedSongsIndexes[i - 1];
         }
     }
 }
