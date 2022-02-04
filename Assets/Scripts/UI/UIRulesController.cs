28ecbcff5de60f607510dad6b728882d1b573952
osx <osx@MacBook-Air-di-osx-8.local>
2022-02-04 12:20:21 +0100

    add screeResolution

--- Assets/Scripts/UI/UIRulesController.cs
+++ Assets/Scripts/UI/UIRulesController.cs
@@ -7,6 +7,15 @@ using UnityEngine;
  * Rule Manager ha aggiornamenti sulle regole e li manda a UIRulesController, RuleeContainerManager si occupa della gestione della regola
  * al suo interno e UIRulesController li controlla e li smista
  */
+
+/*treMezzi,    2160 * 1440     x larghezza y altezza
+  cinqueQuarti,  1280 * 1024
+  quattroTerzi,  1024 * 768
+  sediciNoni,   1920 * 1080      
+  sediciDecimi,  1920 * 1200    
+  HD             2560 * 1080*/
+
+
 public class UIRulesController : MonoBehaviour
 {
     [Header("Animation")]
@@ -17,11 +26,27 @@ public class UIRulesController : MonoBehaviour
     [Header("Settings")]
 
     [SerializeField]
-    private float oddRulesOffset = 125.0f;
+    private float oddRulesOffsetTreMezzi = 125.0f;
+
+    [SerializeField]
+    private float oddRulesOffCinqueQuarti = 125.0f;
+
+    [SerializeField]
+    private float oddRulesOffQuattroTerzi = 125.0f;
+
+    [SerializeField]
+    private float oddRulesOffSediciNoni= 125.0f;
+
+    [SerializeField]
+    private float oddRulesOffSediciDecimi = 125.0f;
+
+    [SerializeField]
+    private float oddRulesOffHD = 125.0f;
 
     [SerializeField]
     private List<RuleContainerManager> ruleContainerManagers = new List<RuleContainerManager>();
 
+    private Resolution screenResolution;
     private Vector3 defaultPos;
     private Vector3 offsettedPos;
 
@@ -39,7 +64,38 @@ public class UIRulesController : MonoBehaviour
     private void Awake()
     {
         defaultPos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
-        offsettedPos = new Vector3(defaultPos.x + oddRulesOffset, defaultPos.y, defaultPos.z);
+        screenResolution = Screen.currentResolution;
+    }
+
+    private void Start()
+    {
+        if (screenResolution.width == 2160 && screenResolution.height == 1440)
+        {
+            offsettedPos = new Vector3(defaultPos.x + oddRulesOffsetTreMezzi, defaultPos.y, defaultPos.z);
+        }
+
+        else if(screenResolution.width == 1280 && screenResolution.height == 1024)
+        {
+            offsettedPos = new Vector3(defaultPos.x + oddRulesOffCinqueQuarti, defaultPos.y, defaultPos.z);
+        }
+        else if (screenResolution.width == 1024 && screenResolution.height == 768)
+        {
+            offsettedPos = new Vector3(defaultPos.x + oddRulesOffQuattroTerzi, defaultPos.y, defaultPos.z);
+        }
+        else if (screenResolution.width ==  1920 && screenResolution.height == 1080)
+        {
+            offsettedPos = new Vector3(defaultPos.x + oddRulesOffSediciNoni, defaultPos.y, defaultPos.z);
+        }
+
+        else if (screenResolution.width == 1920 && screenResolution.height == 1200)
+        {
+            offsettedPos = new Vector3(defaultPos.x + oddRulesOffSediciDecimi, defaultPos.y, defaultPos.z);
+        }
+
+        else if (screenResolution.width == 2560 && screenResolution.height == 1080)
+        {
+            offsettedPos = new Vector3(defaultPos.x + oddRulesOffHD, defaultPos.y, defaultPos.z);
+        }
     }
 
     public void ResetContainers()
