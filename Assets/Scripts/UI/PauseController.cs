78fa257cc6c402da51c653077e4bdd4a2c9daba0
osx <osx@MacBook-Air-di-osx-8.local>
2022-02-07 17:21:03 +0100

    add pauseGame Nav

--- Assets/Scripts/UI/PauseController.cs
+++ Assets/Scripts/UI/PauseController.cs
@@ -1,13 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PauseController : MonoBehaviour, IInputReceiverPause
 {
     [SerializeField]
     private static bool pause;
+
     [SerializeField]
     private GameObject pauseMenu;
+
+
+    [SerializeField]
+    private GameObject homePage;
+
+    [SerializeField]
+    private GameObject settingMenu;
+
+    [SerializeField]
+    public GameObject pausedFirstButton;
+
+    [SerializeField]
+    public GameObject optionsFirstButton;
+
+    [SerializeField]
+    public GameObject optionClosedButton;
 
     private CursorSetter cursorSetter;
 
@@ -22,13 +41,31 @@ public class PauseController : MonoBehaviour, IInputReceiverPause
         Time.timeScale = 1f;
         pause = false;
     }
-
     public void Pause()
     {
         pauseMenu.SetActive(true);
         cursorSetter.SetCursor(CursorType.Menu);
         Time.timeScale = 0f;
         pause = true;
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(pausedFirstButton);
+
+    }
+
+    public void SettingOption()
+    {
+        settingMenu.SetActive(true);
+        homePage.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(optionsFirstButton);
+    }
+
+    public void BackFromSettings()
+    {
+        homePage.SetActive(true);
+        settingMenu.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(optionClosedButton);
     }
 
     public void ReceiveInputPause()
