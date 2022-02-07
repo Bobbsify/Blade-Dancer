7b72f272b40289032584e0c6022ba8ec04aa83d1
Bobbsify <ultra-matty@hotmail.com>
2022-02-07 14:23:14 +0100

    fix cursor directioning

--- Assets/Scripts/Abilities/Shoot.cs
+++ Assets/Scripts/Abilities/Shoot.cs
@@ -37,13 +37,17 @@ public class Shoot : MonoBehaviour, IAbility, IInputReceiverShoot, IGameEntity
         yield return new WaitForSeconds(this.shootCooldown);
         this.canShoot = true;
     }
+    private void Update()
+    {
+        objSpawnPos.rotation = GetRotationToShootAt();
+    }
 
     public void Trigger()
     {
         if (this.canShoot)
         {
             canShoot = false;
-            objSpawnPos.rotation = GetRotationToShootAt();
+            //objSpawnPos.rotation = GetRotationToShootAt();
             GameObject projInstantiated = Instantiate(projectile, this.objSpawnPos); //Create projectile
             projInstantiated.GetComponent<ProjectileController>().SetTeam(Team.Player);
             projInstantiated.transform.parent = projectilesRoot;
