c2fcd697cb9064953d4ee1232024b602f6904cb2
Bobbsify <ultra-matty@hotmail.com>
2022-03-02 11:18:59 +0100

    add commands and fading

---
+++ Assets/Scripts/Scene/ActionAfterSeconds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ActionAfterSeconds : MonoBehaviour
+{
+    [SerializeField]
+    private float timeToExecute;
+
+    [SerializeField]
+    private UnityEvent actions;
+
+    private void Awake()
+    {
+        StartCoroutine(Execute());
+    }
+
+    private IEnumerator Execute() 
+    {
+        yield return new WaitForSeconds(timeToExecute);
+        actions.Invoke();
+    }
+}
