5edcae8060d75d16f8671f71f6dd56740896a541
Bobbsify <ultra-matty@hotmail.com>
2022-03-04 16:09:46 +0100

    fix divineShield

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -112,6 +112,7 @@ public class PlayerController : MonoBehaviour, IGameEntity
     public void FallAnimationCompleted()
     {
         DisableAllAbilities();
+        this.divineShield = false;
         gameManager.PlayerHasFallen();
     }
 
