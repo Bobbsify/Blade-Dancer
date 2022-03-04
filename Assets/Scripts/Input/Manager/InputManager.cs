86ab85519547bf15784de77ba3a5928bd23d0d84
osx <osx@MacBook-Air-di-osx-8.local>
2022-03-04 12:27:38 +0100

    debug add

--- Assets/Scripts/Input/Manager/InputManager.cs
+++ Assets/Scripts/Input/Manager/InputManager.cs
@@ -22,6 +22,7 @@ public class InputManager : MonoBehaviour, IInputManager
     {
       if(Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKeyDown(KeyCode.Joystick1Button1)|| Input.GetKeyDown(KeyCode.Joystick1Button2) || Input.GetKeyDown(KeyCode.Joystick1Button3) || Input.GetKeyDown(KeyCode.Joystick1Button4) || Input.GetKeyDown(KeyCode.Joystick1Button5) || Input.GetKeyDown(KeyCode.Joystick1Button6) || Input.GetKeyDown(KeyCode.Joystick1Button7) || Input.GetKeyDown(KeyCode.Joystick1Button8) || Input.GetKeyDown(KeyCode.Joystick1Button9))
 		{
+			Debug.Log("cursorLock");
 			Cursor.lockState = CursorLockMode.Locked;
         }
 	  else if(Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Mouse1) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.Escape))
