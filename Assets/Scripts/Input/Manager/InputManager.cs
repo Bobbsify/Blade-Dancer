2a9e77afed47db5bd57f06cbfddfbf1934153ee9
osx <osx@MacBook-Air-di-osx-8.local>
2022-03-04 12:17:29 +0100

    preghiamo

--- Assets/Scripts/Input/Manager/InputManager.cs
+++ Assets/Scripts/Input/Manager/InputManager.cs
@@ -17,7 +17,20 @@ public class InputManager : MonoBehaviour, IInputManager
 	{
 		this.inputSenders = GetComponentsInChildren<IInputEntity>(true);
 	}
-	public GameObject GetRoot()
+
+    private void Update()
+    {
+      if(Input.GetKey(KeyCode.Joystick1Button0) || Input.GetKey(KeyCode.Joystick1Button1)|| Input.GetKey(KeyCode.Joystick1Button2) || Input.GetKey(KeyCode.Joystick1Button3) || Input.GetKey(KeyCode.Joystick1Button4) || Input.GetKey(KeyCode.Joystick1Button5) || Input.GetKey(KeyCode.Joystick1Button6) || Input.GetKey(KeyCode.Joystick1Button7) || Input.GetKey(KeyCode.Joystick1Button8) || Input.GetKey(KeyCode.Joystick1Button9))
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+        }
+	  else if(Input.GetKey(KeyCode.Mouse0) || Input.GetKey(KeyCode.Mouse1) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+			Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    public GameObject GetRoot()
 	{
 		return this.searchRoot;
 	}
@@ -36,11 +49,6 @@ public class InputManager : MonoBehaviour, IInputManager
 		{
 			pause.ToggleInput(false);
 		}
-	}
-
-	public bool IsControllerOn()
-	{
-		return isPlayerUsingController;
 	}
 
 	void IInputManager.SetupInput(bool enabled)
