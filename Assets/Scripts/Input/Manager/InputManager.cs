fd48be3f5f17d0c824e704a004fa96250e822181
osx <osx@MacBook-Air-di-osx-8.local>
2022-03-05 11:33:59 +0100

    tab

--- Assets/Scripts/Input/Manager/InputManager.cs
+++ Assets/Scripts/Input/Manager/InputManager.cs
@@ -16,17 +16,16 @@ public class InputManager : MonoBehaviour, IInputManager
 	}
 
     private void Update()
-    {
-      if(Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKeyDown(KeyCode.Joystick1Button1)|| Input.GetKeyDown(KeyCode.Joystick1Button2) || Input.GetKeyDown(KeyCode.Joystick1Button3) || Input.GetKeyDown(KeyCode.Joystick1Button4) || Input.GetKeyDown(KeyCode.Joystick1Button5) || Input.GetKeyDown(KeyCode.Joystick1Button6) || Input.GetKeyDown(KeyCode.Joystick1Button7) || Input.GetKeyDown(KeyCode.Joystick1Button8) || Input.GetKeyDown(KeyCode.Joystick1Button9) || Input.GetAxisRaw("JoystickAxis6") != 0 || Input.GetAxisRaw("JoystickAxis7") != 0 || Input.GetAxisRaw("JoystickAxis9") != 0 || Input.GetAxisRaw("JoystickAxis10") != 0)
+	{ 
+		if(CheckJoystickInput())
 		{
-			Debug.Log("cursorLock");
 			Cursor.lockState = CursorLockMode.Locked;
         }
-	  else if(Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Mouse1) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.Escape) || Input.GetAxisRaw("Mouse X") != 0 || Input.GetAxisRaw("Mouse Y") != 0)
+
+		else if (CheckKeyBoardInput())
         {
-			Debug.Log("cursorUnLock");
 			Cursor.lockState = CursorLockMode.None;
-        }
+		}
     }
 
     public GameObject GetRoot()
@@ -53,5 +52,15 @@ public class InputManager : MonoBehaviour, IInputManager
 	void IInputManager.SetupInput(bool enabled)
 	{
 		this.gameObject.SetActive(enabled);
+	}
+
+	private bool CheckJoystickInput()
+    {
+		return Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKeyDown(KeyCode.Joystick1Button1) || Input.GetKeyDown(KeyCode.Joystick1Button2) || Input.GetKeyDown(KeyCode.Joystick1Button3) || Input.GetKeyDown(KeyCode.Joystick1Button4) || Input.GetKeyDown(KeyCode.Joystick1Button5) || Input.GetKeyDown(KeyCode.Joystick1Button6) || Input.GetKeyDown(KeyCode.Joystick1Button7) || Input.GetKeyDown(KeyCode.Joystick1Button8) || Input.GetKeyDown(KeyCode.Joystick1Button9) || Input.GetAxisRaw("JoystickAxis6") != 0 || Input.GetAxisRaw("JoystickAxis7") != 0 || Input.GetAxisRaw("JoystickAxis9") != 0 || Input.GetAxisRaw("JoystickAxis10") != 0;
+	}
+
+	private bool CheckKeyBoardInput()
+    {
+		return Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Mouse1) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.Escape) || Input.GetAxisRaw("Mouse X") != 0 || Input.GetAxisRaw("Mouse Y") != 0 || Input.GetAxisRaw("TabInputManager") != 0;
 	}
 }
